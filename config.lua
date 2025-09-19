27f029da7bf0346363e065dce63220bcc9ad2882
bigolbug <37874669+bigolbug@users.noreply.github.com>
2025-09-19 13:26:44 -0500

    Updated config

--- config.lua
+++ config.lua
@@ -1,7 +1,7 @@
 afk = {}
 afk.place_count = tonumber(core.settings:get("afk.place_count")) or 15
 afk.place_period = tonumber(core.settings:get("afk.place_period")) or 30
-afk.ban_period = 60 --tonumber(core.settings:get("afk.ban_period")) or 600
+afk.ban_period = tonumber(core.settings:get("afk.ban_period")) or 600
 afk.warning_count = afk.place_count - 5
 afk.metadata = core.get_mod_storage()
 afk.placement_data = {} -- Store metadata: player name -> {timestamps
